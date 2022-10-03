049a0fac2f2cb7dd0245081db57336bd2eeebc77
Kirill Bobrishov <bobryshov@niuitmo.ru>
2022-10-03 08:16:07 +0300

    s

---
+++ cmake-build-debug/CMakeFiles/CionCPP.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CionCPP.dir/main.cpp.obj"
+  "CMakeFiles/CionCPP.dir/main.cpp.obj.d"
+  "CionCPP.exe"
+  "CionCPP.exe.manifest"
+  "CionCPP.pdb"
+  "libCionCPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CionCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
