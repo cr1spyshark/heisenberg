2efe1e4542c337382c8170641c5c908127065230
Kirill Bobrishov <bobryshov@niuitmo.ru>
2022-10-03 08:29:02 +0300

    finish_insetion_sort

--- main.cpp
+++ main.cpp
@@ -8,4 +8,19 @@ int main() {
     for (int i = 0; i < num; i++) {
         std::cin >> array[i];
     }
+
+    for (int i = 1; i < num; i++) {
+        int key = array[i];
+        int j = i - 1;
+
+        while (j >= 0 && array[j] > key) {
+            array[j + 1] = array[j];
+            array[j] = key;
+            j--;
+        }
+    }
+
+    for (int i = 0; i < num; ++i) {
+        std::cout << array[i] << " ";
+    }
 }
